4bcb6a7bd391c6d9ced21b0a0bcf3785399f09ad
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2022-07-18 20:49:49 -0400

    Correccion de Rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/models/especialidades.dart';
 import 'package:flutter_application/page/detalles_doctor.dart';
+import 'package:flutter_application/page/doctores_page.dart';
 import 'package:flutter_application/page/home_page.dart';
 
 import 'package:flutter_application/providers/doctores_provider.dart';
@@ -28,10 +29,10 @@ class AppState extends StatelessWidget {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: HomePage(),  
+        home: DoctoresPage(),  
         
         );
         
--- lib/search/listadoctores.dart
+++ lib/search/listadoctores.dart
@@ -6,69 +6,6 @@ import 'package:flutter_application/providers/especialidades_provider.dart';
 import 'package:flutter_application/search/search_delegate.dart';
 import 'package:provider/provider.dart';
 
-class DoctoresPage extends StatefulWidget {
-  const DoctoresPage({Key? key}) : super(key: key);
-
-  @override
-  State<DoctoresPage> createState() => _DoctoresPage();
-}
-
-class _DoctoresPage extends State<DoctoresPage> {
-  String opcionPorDefecto = 'Especialidad';
-  String? especialidad = '';
-  String? e = '';
-  @override
-  Widget build(BuildContext context) {
-    final especialidadesProvider = Provider.of<EspecialidadesProvider>(context);
-    final items = especialidadesProvider.especialidadesDisponibles;
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Doctores'),
-          actions: [
-            
-            DropdownButton<Especialidades>(
-                items: items
-                    .map<DropdownMenuItem<Especialidades>>((Especialidades a) {
-                  return DropdownMenuItem<Especialidades>(
-                    value: a,
-                    child: Text(a.nombre),
-                  );
-                }).toList(),
-                onChanged: (Especialidades? a) => setState(() {
-                      if (a?.nombre == '...') {
-                        especialidad = '';
-                        e = '';
-                        opcionPorDefecto = 'Especialidad';
-                      } else {
-                        e = 'e/';
-                        especialidad = a?.nombre;
-                        opcionPorDefecto = especialidad!;
-                      }
-                    }),
-                hint: Text(opcionPorDefecto)
-                ),
-             IconButton(
-              onPressed: ()=> showSearch(context: context, delegate: DoctorSearchDelegate()), 
-              icon: Icon(Icons.search_outlined)
-              ),
-
-          ],
-        ),
-        body: FutureBuilder(
-            future: Doctor.fetchDoctores(
-                'http://10.0.2.2:3000/doctor/get/' + e! + especialidad!),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                return ListaDoctores(snapshot.data);
-              }
-
-              ;
-            }));
-  }
-}
-
 class ListaDoctores extends StatelessWidget {
   final List<Doctor> doctores;
 
